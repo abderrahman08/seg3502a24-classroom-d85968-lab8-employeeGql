25ef9b7c86c892d0f1164989b7bb0a886954836f
abderrahman08 <91130555+abderrahman08@users.noreply.github.com>
2024-11-10 19:07:20 -0500

    updated app

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,40 +1,35 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, map, Observable} from "rxjs";
-import {Employee} from "../model/employee";
-import {Apollo} from "apollo-angular";
-import {gql} from "@apollo/client/core";
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { Employee } from '../model/employee';
+import { Apollo } from 'apollo-angular';
+import { gql } from '@apollo/client/core';
 
 const GET_EMPLOYEES = gql`
-query {
-  employees {
-    id
-    name
-    dateOfBirth
-    city
-    salary
-    gender
-    email
+  query {
+    employees {
+      id
+      name
+      dateOfBirth
+      city
+      salary
+      gender
+      email
+    }
   }
-}
 `;
 
 const ADD_EMPLOYEE = gql`
-mutation addEmployee($name: String!,
-  $dateOfBirth: String!,
-  $city: String!,
-  $salary: Float!,
-  $gender: String!,
-  $email: String!) {
-  newEmployee(createEmployeeInput: {name: $name, dateOfBirth: $dateOfBirth, city: $city, salary: $salary, gender: $gender, email: $email}) {
-    id
-    name
-    dateOfBirth
-    city
-    salary
-    gender
-    email
+  mutation addEmployee($input: CreateEmployeeInput!) {
+    addEmployee(input: $input) {
+      id
+      name
+      dateOfBirth
+      city
+      salary
+      gender
+      email
+    }
   }
-}
 `;
 
 @Injectable({
@@ -44,29 +39,37 @@ export class EmployeeService {
   employees$: BehaviorSubject<readonly Employee[]> = new BehaviorSubject<readonly Employee[]>([]);
 
   constructor(private apollo: Apollo) {
-    this.apollo.watchQuery<any>({query: GET_EMPLOYEES}).valueChanges.pipe(
-      map(({data, loading}) => {
+    // Fetch employees and update the BehaviorSubject
+    this.apollo.watchQuery<any>({ query: GET_EMPLOYEES }).valueChanges.pipe(
+      map(({ data }) => {
         this.employees$.next(data.employees);
       })
     ).subscribe();
   }
 
+  // Observable getter for employees
   get $(): Observable<readonly Employee[]> {
     return this.employees$.asObservable();
   }
 
+  // Add a new employee and update the BehaviorSubject
   addEmployee(employee: Employee) {
-    return this.apollo.mutate<any>({mutation: ADD_EMPLOYEE, variables: {
+    const input = {
       name: employee.name,
       dateOfBirth: employee.dateOfBirth,
       city: employee.city,
       salary: employee.salary,
       gender: employee.gender,
       email: employee.email
-    }}).pipe(
-          map(({data, loading}) => {
-            this.employees$.next([...this.employees$.getValue(), data.newEmployee]);
-          })
-    )
+    };
+
+    return this.apollo.mutate<any>({
+      mutation: ADD_EMPLOYEE,
+      variables: { input }
+    }).pipe(
+      map(({ data }) => {
+        this.employees$.next([...this.employees$.getValue(), data.addEmployee]);
+      })
+    );
   }
 }
